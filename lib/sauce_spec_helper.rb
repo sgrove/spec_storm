8c3795704a1af1e072c723429985d13559a1818e
Sean Grove <sean@chuwe.com>
2010-01-27 16:35:18 -0800

    Only patch the selenium driver once per process.

--- lib/sauce_spec_helper.rb
+++ lib/sauce_spec_helper.rb
@@ -15,13 +15,16 @@ module SauceSpecs
     end
     
     Selenium::Client::Driver.class_eval do
-      alias :original_open :open
-      
-      def open(url)
-        # TODO: Regex stuff to append query properly
-        new_url = url.include?("db_prefix=") ? url :"#{url}?db_prefix=#{db_prefix}"
-        puts "Patching url, opening #{new_url}"
-        original_open( new_url )
+      # Make sure we don't define this twice
+      unless self.instance_methods.include? "original_open"
+        alias :original_open :open
+        
+        def open(url)
+          # TODO: Regex stuff to append query properly
+          new_url = url.include?("db_prefix=") ? url :"#{url}?db_prefix=#{db_prefix}"
+          puts "Patching url, opening #{new_url}"
+          original_open( new_url )
+        end
       end
     end
   end
